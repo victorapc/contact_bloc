539fff1b79919340932f79138d83e98ed5ee82bd
victor <victor.alexandre@atlantasistemas.com>
2023-07-26 20:53:43 -0300

    Bloc configurando loader e error.

--- lib/feature/bloc_example/contacts/list/contact_bloc/contact_list_bloc.dart
+++ lib/feature/bloc_example/contacts/list/contact_bloc/contact_list_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:contact_bloc/models/contact_model.dart';
 import 'package:contact_bloc/repositories/contacts_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -18,8 +20,14 @@ class ContactListBloc extends Bloc<ContactListEvent, ContactListState> {
 
   Future<void> _findAll(
       _ContactListEventFindAll event, Emitter<ContactListState> emit) async {
-    final contacts = await _repository.findAll();
-
-    emit(ContactListState.data(contacts: contacts));
+    try {
+      emit(ContactListState.loading());
+      final contacts = await _repository.findAll();
+      await Future.delayed(const Duration(seconds: 1));
+      emit(ContactListState.data(contacts: contacts));
+    } catch (e, s) {
+      log('Erro ao buscar contatos.', error: e, stackTrace: s);
+      emit(ContactListState.error(error: 'Erro ao buscar contatos.'));
+    }
   }
 }
--- lib/feature/bloc_example/contacts/list/contacts_list_page.dart
+++ lib/feature/bloc_example/contacts/list/contacts_list_page.dart
@@ -1,3 +1,4 @@
+import 'package:contact_bloc/core/widget/loader.dart';
 import 'package:contact_bloc/feature/bloc_example/contacts/list/contact_bloc/contact_list_bloc.dart';
 import 'package:contact_bloc/models/contact_model.dart';
 import 'package:flutter/material.dart';
@@ -12,37 +13,71 @@ class ContactsListPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Contacts Page'),
       ),
-      body: CustomScrollView(
-        slivers: [
-          SliverFillRemaining(
-            child: Column(
-              children: [
-                BlocSelector<ContactListBloc, ContactListState,
-                    List<ContactModel>>(
-                  selector: (state) {
-                    return state.maybeWhen(
-                      data: (contacts) => contacts,
-                      orElse: () => [],
-                    );
-                  },
-                  builder: (_, contacts) {
-                    return ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: contacts.length,
-                      itemBuilder: (context, index) {
-                        final contact = contacts[index];
-                        return ListTile(
-                          title: Text(contact.name),
-                          subtitle: Text(contact.email),
-                        );
-                      },
-                    );
-                  },
+      body: BlocListener<ContactListBloc, ContactListState>(
+        listenWhen: (previous, current) {
+          return current.maybeWhen(
+            error: (error) => true,
+            orElse: () => false,
+          );
+        },
+        listener: (context, state) {
+          state.whenOrNull(
+            error: (error) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    error,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  backgroundColor: Colors.red,
                 ),
-              ],
+              );
+            },
+          );
+        },
+        child: CustomScrollView(
+          slivers: [
+            SliverFillRemaining(
+              child: Column(
+                children: [
+                  Loader<ContactListBloc, ContactListState>(
+                    selector: (state) {
+                      return state.maybeWhen(
+                        loading: () => true,
+                        orElse: () => false,
+                      );
+                    },
+                  ),
+                  BlocSelector<ContactListBloc, ContactListState,
+                      List<ContactModel>>(
+                    selector: (state) {
+                      return state.maybeWhen(
+                        data: (contacts) => contacts,
+                        orElse: () => [],
+                      );
+                    },
+                    builder: (_, contacts) {
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: contacts.length,
+                        itemBuilder: (context, index) {
+                          final contact = contacts[index];
+                          return ListTile(
+                            title: Text(contact.name),
+                            subtitle: Text(contact.email),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
